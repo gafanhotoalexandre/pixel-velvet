5980f96c45b33a84454c73da690ed975e28c6c74
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-07 01:27:12 -0300

    desenvolvendo a sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,16 @@
+import { Sidebar } from '@/components/shared/Sidebar'
+
 interface LayoutProps {
   children: React.ReactNode
 }
 export default function Layout({ children }: LayoutProps) {
   return (
     <main className="root">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* MobileNav */}
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
