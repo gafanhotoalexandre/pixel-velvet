eb5f36dd962e05c4397db28b9674aa95766c0629
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-08 01:06:34 -0300

    trabalhando nas actions para User

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,40 @@
 'use server'
+
+import { revalidatePath } from 'next/cache'
+
+import User from '../models/user.model'
+import { connectToDatabase } from '../database/mongoose'
+import { handleError } from '../utils'
+
+// CREATE
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase()
+
+    const newUser = await User.create(user)
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// READ
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase()
+
+    const user = await User.findOne({ clerkId: userId })
+
+    if (!user) throw new Error('Usuário não encontrado.')
+
+    return JSON.parse(JSON.stringify(user))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// UPDATE
+export async function updateUser(clerkId: string, user: UpdateUserParams) {}
+
+// DELETE
+export async function deleteUser(clerkId: string) {}
