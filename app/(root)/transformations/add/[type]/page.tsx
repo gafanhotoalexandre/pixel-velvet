4520d0a41482539d404e9f8dee60fc6d98faeb5f
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-08 22:51:27 -0300

    desenvolvendo o componente TransformationForm

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,12 +1,32 @@
+import { redirect } from 'next/navigation'
+import { auth } from '@clerk/nextjs'
+
 import { Header } from '@/components/shared/Header'
+import { TransformationForm } from '@/components/shared/TransformationForm'
 
+import { getUserById } from '@/lib/actions/user.actions'
 import { transformationTypes } from '@/constants'
+import { IUser } from '@/lib/models/user.model'
 
-export default function AddTransformationTypePage({
+export default async function AddTransformationTypePage({
   params: { type },
 }: SearchParamProps) {
+  const { userId } = auth()
   const transformation = transformationTypes[type]
+
+  if (!userId) redirect('/sign-in')
+
+  const user: IUser = await getUserById(userId)
   return (
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance!}
+      />
+    </>
   )
 }
