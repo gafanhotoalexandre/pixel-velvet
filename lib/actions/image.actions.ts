d3c5552509bf6cd1b976180916f82e26a1c5e9d1
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-18 23:44:32 -0300

    finalizando as actions de image

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,17 +1,38 @@
 'use server'
 
 import { revalidatePath } from 'next/cache'
-import { connectToDatabase } from '../database/mongoose'
 import { handleError } from '../utils'
+
+import { connectToDatabase } from '../database/mongoose'
+import User, { IUser } from '../models/user.model'
+import Image, { IImage } from '../models/image.model'
+import { redirect } from 'next/navigation'
+
+// utils
+const populateUser = (query: any) =>
+  query.populate({
+    path: 'author',
+    model: User,
+    select: '_id firstName lastName',
+  })
 
 // ADD IMAGE
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase()
 
+    const author: IUser | null = await User.findById(userId)
+
+    if (!author) throw new Error('Usuário não encontrado.')
+
+    const newImage: IImage = await Image.create({
+      ...image,
+      author: author._id,
+    })
+
     revalidatePath(path)
 
-    return JSON.parse(JSON.stringify(image))
+    return JSON.parse(JSON.stringify(newImage))
   } catch (error) {
     handleError(error)
   }
@@ -22,9 +43,20 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase()
 
+    const imageToUpdate = await Image.findById(image._id)
+
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId)
+      throw new Error('Não autorizado ou imagem não encontrada.')
+
+    const updatedImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
+      { new: true }
+    )
+
     revalidatePath(path)
 
-    return JSON.parse(JSON.stringify(image))
+    return JSON.parse(JSON.stringify(updatedImage))
   } catch (error) {
     handleError(error)
   }
@@ -35,11 +67,11 @@ export async function deleteImage(imageId: string) {
   try {
     await connectToDatabase()
 
-    // revalidatePath(path)
-
-    // return JSON.parse(JSON.stringify(image))
+    await Image.findByIdAndDelete(imageId)
   } catch (error) {
     handleError(error)
+  } finally {
+    redirect('/')
   }
 }
 
@@ -48,9 +80,11 @@ export async function getImageById(imageId: string) {
   try {
     await connectToDatabase()
 
-    // revalidatePath(path)
+    const image = await populateUser(Image.findById(imageId))
 
-    // return JSON.parse(JSON.stringify(image))
+    if (!image) throw new Error('Imagem não encontrada.')
+
+    return JSON.parse(JSON.stringify(image))
   } catch (error) {
     handleError(error)
   }
